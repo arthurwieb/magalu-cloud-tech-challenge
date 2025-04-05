b09bc475260e19d339b36a90773943d9a3cfadcc
arthurwieb <arthur.wieb@gmail.com>
2025-04-04 23:52:30 -0300

    criação de classe p/ gerar pulses

---
+++ pulsegenerator/pulsegenerator.go
@@ -0,0 +1,56 @@
+package pulsegenerator
+
+import (
+	"math/rand"
+)
+
+var TENANT_ID = []string{"1", "2", "3", "4", "5"}
+
+type Emitter interface {
+	generateRandomPulse() Pulse
+}
+
+type Pulse struct {
+	Tenant     string
+	ProductSKU string
+	UsedAmount float64
+	UsageUnit  string
+	Day        int // em um cenário real, seria um timestamp
+}
+
+// mock de dados de um pulse gerado
+func GenerateRandomPulse(day int) Pulse {
+	tenantName := "tenant_" + TENANT_ID[rand.Intn(len(TENANT_ID))]
+	amount := rand.Float64() * float64(rand.Intn(10000)+1) // ajustar isso, parece super não efetivo
+
+	var newPulse Pulse
+	if rand.Intn(2) > 0 {
+		newPulse = generateStoragePulse(tenantName, amount, day)
+	} else {
+		newPulse = generateNetworkPulse(tenantName, amount, day)
+	}
+
+	return newPulse
+}
+
+func generateStoragePulse(Tenant string, amount float64, day int) Pulse {
+
+	return Pulse{
+		Tenant:     Tenant,
+		ProductSKU: "storage_1gb",
+		UsedAmount: amount * 60,
+		UsageUnit:  "GB x seg",
+		Day:        day,
+	}
+}
+
+func generateNetworkPulse(Tenant string, amount float64, day int) Pulse {
+
+	return Pulse{
+		Tenant:     Tenant,
+		ProductSKU: "network_egress",
+		UsedAmount: amount,
+		UsageUnit:  "MB",
+		Day:        day,
+	}
+}
